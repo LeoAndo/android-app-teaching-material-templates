dccc3b0a22a1d35816aa810562c61302184ede39
LeoAndo <leo.777.info@gmail.com>
2021-09-22 00:33:43 +0900

    #1 javaとkotlinバージョンどちらも対応したTemplateプロジェクトの作成

---
+++ BasicAppSample/app/src/kotlinVersion/java/com/example/basicappsample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.basicappsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textHello: TextView = findViewById(R.id.text_hello)
+        textHello.text = "Hello, Android Kotlin"
+    }
+}
