00c314eabb25c00b9c8aed4fe167ff92ab24ba5f
LeoAndo <leo.777.info@gmail.com>
2021-09-24 17:45:41 +0900

    JetpackCompose用のプロジェクトを作成する #2

--- BasicComposeAppSample/app/src/main/java/com/example/basiccomposeappsample/MainActivity.kt
+++ BasicComposeAppSample/app/src/main/java/com/example/basiccomposeappsample/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.basiccomposeappsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccomposeappsample.ui.theme.BasicComposeAppSampleTheme
 
@@ -17,7 +23,7 @@ class MainActivity : ComponentActivity() {
             BasicComposeAppSampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("Compose!!")
                 }
             }
         }
@@ -26,13 +32,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Hello $name!", color = Color.Cyan)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BasicComposeAppSampleTheme {
-        Greeting("Android")
+        Greeting("Compose!!")
     }
 }
