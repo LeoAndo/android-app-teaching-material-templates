1f6e8cd9a8e7dedce3bd1f7613aa723f9b5c02a9
LeoAndo <leo.777.info@gmail.com>
2021-09-27 14:37:06 +0900

    add anim helper

---
+++ BasicAppSample/app/src/main/java/com/example/basicappsample/helpers/AnimationHelper.java
@@ -0,0 +1,39 @@
+package com.example.basicappsample.helpers;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+import androidx.transition.TransitionManager;
+
+import com.google.android.material.transition.MaterialFade;
+
+public final class AnimationHelper {
+    private AnimationHelper() {
+    }
+
+    /**
+     * 点滅アニメーションを行う.
+     *
+     * @param view アニメーションを適用させるView
+     */
+    public static void startAlphaAnimation(View view) {
+        final float fromAlpha = 0.0f, toAlpha = 1.0f;
+        final long durationMillis = 800, startOffset = 1000;
+        Animation animation = new AlphaAnimation(fromAlpha, toAlpha);
+        animation.setDuration(durationMillis);
+        animation.setStartOffset(startOffset);
+        animation.setRepeatMode(Animation.REVERSE);
+        animation.setRepeatCount(Animation.INFINITE);
+        view.startAnimation(animation);
+    }
+    // 表示 / 非表示アニメーション処理
+    private void startFadeAnimation(boolean isVisible, ViewGroup vg, View targetView) {
+        MaterialFade fade = new MaterialFade();
+        fade.setDuration(500L);
+        TransitionManager.beginDelayedTransition(vg, fade);
+        int visibility = (isVisible) ? View.VISIBLE : View.INVISIBLE;
+        targetView.setVisibility(visibility);
+    }
+}
