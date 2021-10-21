3b50d5528d8ac4226c15c3af1fefde010d0072ea
LeoAndo <leo.777.info@gmail.com>
2021-10-21 13:03:46 +0900

    Keyboard非表示対応

--- BasicAppSample/app/src/javaVersion/java/com/example/basicappsample/MainActivity.java
+++ BasicAppSample/app/src/javaVersion/java/com/example/basicappsample/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.basicappsample;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.basicappsample.databinding.ActivityMainBinding;
 import com.example.basicappsample.helpers.AnimationHelper;
+import com.example.basicappsample.helpers.KeyboardHelpable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements KeyboardHelpable {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         binding.textHello.setText("Hello, Android Java");
         AnimationHelper.startAlphaAnimation(binding.textHello);
+        binding.editEmail.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                hideKeyboard(MainActivity.this);
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 }
--- BasicAppSample/app/src/kotlinVersion/java/com/example/basicappsample/MainActivity.kt
+++ BasicAppSample/app/src/kotlinVersion/java/com/example/basicappsample/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.basicappsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import com.example.basicappsample.databinding.ActivityMainBinding
 import com.example.basicappsample.helpers.AnimationHelper
+import com.example.basicappsample.helpers.KeyboardHelpable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), KeyboardHelpable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater).also {
@@ -13,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         }
         binding.textHello.text = "Hello, Android Kotlin"
         AnimationHelper.startAlphaAnimation(binding.textHello)
+
+        binding.editEmail.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                hideKeyboard()
+                return@setOnKeyListener true
+            } else {
+                return@setOnKeyListener false
+            }
+        }
     }
 }
