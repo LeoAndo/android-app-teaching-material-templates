731b3c28c9886e5fc4b8c37639a700463f6b50d5
LeoAndo <leo.777.info@gmail.com>
2022-02-22 20:41:27 +0900

    add TextInputEditText handling

--- M3BasicSample/app/src/main/java/com/example/m3basicsample/models/User.kt
+++ M3BasicSample/app/src/main/java/com/example/m3basicsample/models/User.kt
@@ -2,4 +2,4 @@ package com.example.m3basicsample.models
 
 import java.io.Serializable
 
-data class User(val name: String, val age: Int): Serializable
+internal data class User(val name: String, val age: Int): Serializable
