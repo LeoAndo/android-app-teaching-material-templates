f62f26b3d436c88512d38ec16e2754502893f48a
LeoAndo <leo.777.info@gmail.com>
2022-02-23 02:16:06 +0900

    add simpleAPp

---
+++ M3SimpleApp/app/src/main/java/com/example/m3simpleapp/core/ApplicationLocalesHandler.kt
@@ -0,0 +1,25 @@
+package com.example.m3simpleapp.core
+
+import android.app.LocaleManager
+import android.os.Build
+import android.os.LocaleList
+import androidx.annotation.RequiresApi
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+//@TargetApi(Build.VERSION_CODES.TIRAMISU)
+internal class ApplicationLocalesHandler constructor(
+    val localeManager: LocaleManager,
+) {
+    inline val supportLanguageTags
+        get() = localeManager.applicationLocales.toLanguageTags()
+
+    inline var applicationLocales: LocaleList
+        get() = localeManager.applicationLocales
+        set(locales) {
+            localeManager.applicationLocales = locales
+        }
+
+    companion object {
+        private const val TAG = "ApplicationLocalesHandler"
+    }
+}
