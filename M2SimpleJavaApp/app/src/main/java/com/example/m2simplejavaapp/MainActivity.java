204307ff4ec8342f91079ecc94034d3aa5ca6e02
LeoAndo <leo.777.info@gmail.com>
2023-01-06 18:21:37 +0900

    edge-to-edge

--- M2SimpleJavaApp/app/src/main/java/com/example/m2simplejavaapp/MainActivity.java
+++ M2SimpleJavaApp/app/src/main/java/com/example/m2simplejavaapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.m2simplejavaapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.WindowCompat;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        // onCreate(android.os.Bundle, android.os.PersistableBundle) で呼び出すと、Insetの計算が行われる
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
